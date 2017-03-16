b0a306e4645d0f5d1cae641d4c1aede55720eddc
ssttevee <slam@akshonesports.com>
2017-03-16 15:15:38 -0700

    version 0.1.1 - make directory in cache before starting - fixed unnatual quoting - fixed copying govendor bin to cache as "*"

--- run.sh
+++ run.sh
@@ -6,12 +6,17 @@ if ! which go; then
 	exit 1
 fi
 
+# make govendor-sync directory in the cache
+if [ ! -d "$WERCKER_CACHE_DIR"/govendor-sync ]; then
+	mkdir -p "$WERCKER_CACHE_DIR"/govendor-sync/bin "$WERCKER_CACHE_DIR"/govendor-sync/vendor
+fi
+
 # install govendor or copy from cache
-cp "$WERCKER_CACHE_DIR/govendor-sync/bin/*" "$GOPATH/bin/"
+cp "$WERCKER_CACHE_DIR"/govendor-sync/bin/* "$GOPATH"/bin/
 
 if ! which govendor; then
 	go get -u github.com/kardianos/govendor
-	cp -f "$GOPATH/bin/govendor" "$WERCKER_CACHE_DIR/govendor-sync/bin/*"
+	cp -f "$GOPATH"/bin/govendor "$WERCKER_CACHE_DIR"/govendor-sync/bin/
 fi
 
 # check for existence of vendor.json
@@ -21,8 +26,8 @@ if [ ! -e vendor/vendor.json ]; then
 fi
 
 # restore vendor cache and sync
-cp -r "$WERCKER_CACHE_DIR/govendor-sync/vendor/*" vendor/
+cp -r "$WERCKER_CACHE_DIR"/govendor-sync/vendor/* vendor/
 
 govendor sync
 
-cp -r vendor/*/ "$WERCKER_CACHE_DIR/govendor-sync/vendor/"
+cp -r vendor/*/ "$WERCKER_CACHE_DIR"/govendor-sync/vendor/
